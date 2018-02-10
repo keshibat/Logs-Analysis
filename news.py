0401082bb4192e141946f82fd50100c9da3f9879
keshibat <kensukeshibata@.com>
2018-02-10 12:57:31 +0900

    print the results in a nice format(question1&2)

--- news.py
+++ news.py
@@ -1,5 +1,9 @@
-#! /usr/bin/env pythoimport psycopg2
-import time
+#!/usr/bin/env python
+#Project Logs Analysis
+
+import psycopg2
+import datetime
+
 
 DBNAME="news"
 
@@ -15,12 +19,14 @@ query3=("""SELECT time, perc FROM percent_error WHERE perc >= 1;""")
 
 
 
+
 def popluar_article(query1):
     db = psycopg2.connect("dbname=news")
     c = db.cursor()
     c.execute(query1)
     results = c.fetchall()
-    print(results)
+    for e in results:
+        print( "{0} -- {1} views".format(e[0], e[1]))
     db.close()
 
 def poplular_author(query2):
@@ -28,7 +34,8 @@ def poplular_author(query2):
     c = db.cursor()
     c.execute(query2)
     results = c.fetchall()
-    print(results)
+    for e in results:
+        print( "{0} -- {1} views".format(e[0], e[1]))
     db.close()
 
 def error_percent(query3):
@@ -36,15 +43,18 @@ def error_percent(query3):
     c = db.cursor()
     c.execute(query3)
     results = c.fetchall()
-    print(results)
+    for e in results:
+        print (e)
     db.close()
 
 
 if __name__ == '__main__':
     print ("The 3 most poplular articles of all time are:")
     popluar_article(query1)
+    print("\n")
     print ("The most popular article authors of all time are")
     poplular_author(query2)
+    print("\n")
     print ("Days did more than 1% of requests lead to errors")
     error_percent(query3)
 
